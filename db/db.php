deff066dfbb2f8761cf2b5dc25dde8c3b06d6edd
91neftci <azer.mirzeyevv@gmail.com>
2024-12-25 17:57:33 +0400

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,7 @@
+﻿<?php
+$host = "sql204.infinityfree.com";
+$username = "if0_37978519";
+$password = "QSvPJWsu8aPP";
+$db = "if0_37978519_evler";
+$dbcon = mysqli_connect($host,$username,$password,$db);
+?>
