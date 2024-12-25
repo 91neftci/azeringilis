50b78105def1e7a3856fbaade86d2f28c90cde13
91neftci <azer.mirzeyevv@gmail.com>
2024-12-25 18:07:06 +0400

    Delete db/db.php

--- db/db.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-$host = "sql204.infinityfree.com";
-$username = "if0_37978519";
-$password = "QSvPJWsu8aPP";
-$db = "if0_37978519_evler";
-$dbcon = mysqli_connect($host,$username,$password,$db);
-?>
