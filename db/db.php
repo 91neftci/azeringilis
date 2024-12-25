bb8d223fce947d56a807d4e76968977296124651
91neftci <azer.mirzeyevv@gmail.com>
2024-12-25 18:07:17 +0400

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,14 @@
+﻿<?php
+$host = "sql204.infinityfree.com";
+$username = "if0_37978519";
+$password = "QSvPJWsu8aPP";
+$db = "if0_37978519_evler";
+
+$conn = new mysqli($host, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Bağlantı hatası: " . $conn->connect_error);
+}
+echo "Bağlantı başarılı!";
+
+?>
